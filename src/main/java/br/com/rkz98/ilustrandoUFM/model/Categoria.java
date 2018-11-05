2bdefe3e3adcdd4e54022c1dda51e638692b7225
Kauê Rodrigues dos Santos <kauerodrigues98@gmail.com>
2018-11-05 00:20:15 -0200

    Update src/main/java/br/com/rkz98/ilustrandoUFM/model/Categoria.java

---
+++ src/main/java/br/com/rkz98/ilustrandoUFM/model/Categoria.java
@@ -0,0 +1,43 @@
+package br.com.rkz98.ilustrandoUFM.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	public Categoria() {
+
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
