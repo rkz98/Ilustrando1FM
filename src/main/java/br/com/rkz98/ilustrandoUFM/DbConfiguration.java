52f16366c0be63a30dcc5d53e9dc54e71541f972
Kauê Rodrigues dos Santos <kauerodrigues98@gmail.com>
2018-11-05 00:19:46 -0200

    Update src/main/java/br/com/rkz98/ilustrandoUFM/DbConfiguration.java

---
+++ src/main/java/br/com/rkz98/ilustrandoUFM/DbConfiguration.java
@@ -0,0 +1,35 @@
+package br.com.rkz98.ilustrandoUFM;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DbConfiguration {
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/ilustrando1fm");
+		dataSource.setUsername("root");
+		dataSource.setPassword("rkz98");
+		return dataSource;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+}
